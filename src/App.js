42387d3d65ff51c8521a7c32ee3d984522ae044b
mufeed <mufeedmangalad@gmail.vom>
2024-01-03 13:04:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/admin/Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/user/home" element={<UserHome />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/user/Sidebar/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import sty from "./style.module.css";
+const Sidebar = () => {
+  return <div className={sty.sidebar_main_section}>sidebar</div>;
+};
+
+export default Sidebar;
